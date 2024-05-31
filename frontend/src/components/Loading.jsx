2cba79fdd1dbc0d069ff77cc6023f9fb3a9c5bda
shanmukh-sai2003 <koradashanmukhasai10@gmail.com>
2024-05-31 12:29:20 +0530

    Update loader for chat page

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,6 +1,6 @@
 function Loading() {
     return (
-        <div className='h-[100%] w-[67vw] flex justify-center items-center bg-slate-900'>
+        <div className='h-[100%] flex justify-center items-center bg-slate-900'>
             <div className="w-16 h-16 border-t-4 border-blue-500 rounded-full animate-spin"></div>
         </div>
     );
