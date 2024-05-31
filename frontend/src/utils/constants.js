ca726c5a8b6bf423a520e20fdf3e2616d7eb6f2b
shanmukh-sai2003 <koradashanmukhasai10@gmail.com>
2024-05-31 12:03:00 +0530

    Update api url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000/api/v1';
-export const API_BASE_URL = 'http://localhost:3000';
+export const API_URL = 'https://chit-chat-backend-jt9j.onrender.com/api/v1';
+export const API_BASE_URL = 'https://chit-chat-backend-jt9j.onrender.com';
